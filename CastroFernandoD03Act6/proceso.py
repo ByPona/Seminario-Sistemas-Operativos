3d351255436142f3c9dd59e7054bd5906c2e98af
Fernando Castro <84426610+ByPona@users.noreply.github.com>
2022-05-10 21:52:18 -0500

    Add files via upload

---
+++ CastroFernandoD03Act6/proceso.py
@@ -0,0 +1,83 @@
+class Proceso(object):
+
+    def __init__(self, id, tiempo, transcurrido, operacion, operacion_resultado, tiempo_bloqueado, tiempo_de_llegada, tiempo_de_finalizacion, tiempo_de_retorno, tiempo_de_respuesta, tiempo_de_espera, tiempo_de_servicio, hora_inicial, tiempo_que_lleva_bloqueado):
+        self.id = id
+        self.tiempo = tiempo
+        self.transcurrido = transcurrido
+        self.operacion = operacion
+        self.operacion_resultado = operacion_resultado
+        self.tiempo_bloqueado = tiempo_bloqueado
+        self.tiempo_de_llegada = tiempo_de_llegada
+        self.tiempo_de_finalizacion = tiempo_de_finalizacion
+        self.tiempo_de_retorno = tiempo_de_retorno
+        self.tiempo_de_respuesta = tiempo_de_respuesta
+        self.tiempo_de_espera = tiempo_de_espera
+        self.tiempo_de_servicio = tiempo_de_servicio
+        self.hora_inicial = hora_inicial
+        self.tiempo_que_lleva_bloqueado = tiempo_que_lleva_bloqueado
+
+    def darnuevo(self):
+        return "Programa No.: %s\nTiempo Estimado: %s\nTiempo Transcurrido: %s" % (self.id, self.tiempo, self.transcurrido)
+    
+    def darid(self):
+        return self.id
+    
+    def dartiempo(self):
+        return self.tiempo
+
+    def dartranscurrido(self):
+        return self.transcurrido
+    
+    def daroperacion(self):
+        return self.operacion
+
+    def darresultado(self):
+        return self.operacion_resultado
+
+    def darbloqueado(self):
+        return self.tiempo_bloqueado
+
+    def darhorainicial(self):
+        return self.hora_inicial
+    
+    def dartiempoquellevabloqueado(self):
+        return self.tiempo_que_lleva_bloqueado
+
+    def establecertranscurrido(self, transcurrido):
+        self.transcurrido = transcurrido
+
+    def establecertiempobloqueado(self, bloqueado):
+        self.tiempo_bloqueado = bloqueado
+
+    def establecerresultado(self, resultado):
+        self.operacion_resultado = resultado
+
+    def establecerllegada(self, llegada):
+        self.tiempo_de_llegada = llegada
+
+    def establecerfinalizacion(self, finalizacion):
+        self.tiempo_de_finalizacion = finalizacion
+    
+    def establecerretorno(self, retorno):
+        self.tiempo_de_retorno = retorno
+
+    def establecerrespuesta(self, respuesta):
+        self.tiempo_de_respuesta = respuesta
+    
+    def establecerespera(self, espera):
+        self.tiempo_de_espera = espera
+
+    def establecerservicio(self, servicio):
+        self.tiempo_de_servicio = servicio
+
+    def establecerhorainicial(self, inicial):
+        self.hora_inicial = inicial
+
+    def establecertiempoquellevabloqueado(self, bloque):
+        self.tiempo_que_lleva_bloqueado = bloque
+
+    def __str__(self):
+        return "Programa No.: %s\nTiempo Estimado: %s\nTranscurrido: %s" % (self.id, self.tiempo, self.transcurrido)
+
+    def darfinal(self):
+        return "Programa No.: %s\nTiempo Estimado: %s\nTranscurrido: %s\nOperacion: %s\nRespuesta: %s\nTiempos\nLlegada: %s\nFinalizacion: %s\nRetorno: %s\nRespuesta: %s\nEspera: %s\nServicio: %s\n" % (self.id, self.tiempo,self.transcurrido,self.operacion, self.operacion_resultado,self.tiempo_de_llegada,self.tiempo_de_finalizacion, self.tiempo_de_retorno, self.tiempo_de_respuesta, self.tiempo_de_espera, self.tiempo_de_servicio)
